6361bf5329d262ef678d50183659e19d227909a0
Alain Tresor <tresoralain35@gmail.com>
2023-01-30 20:11:53 +0200

    add cors

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ import bodyParser from "body-parser";
 import mongoConnect from './src/config/db.config.js'
 import dotenv from 'dotenv'
 import router from "./src/routers/index.js";
+import cors from 'cors'
 dotenv.config()
 
-const port= 3000 ;
+const port = 3000;
 
 
-const app= express();
+const app = express();
 mongoConnect();
 
-app.use(bodyParser.json({limit: '10mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+app.use(cors())
+app.use(bodyParser.json({ limit: '10mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.status(200).json({
-        message :"let's get started ",
-        status:200
+        message: "let's get started ",
+        status: 200
     })
 });
 
-app.use('/api/v1',router)
+app.use('/api/v1', router)
 
-app.use((req,res) =>{
+app.use((req, res) => {
     res.status(404).json({
-        message:"endpoint not found",
-        status:404
+        message: "endpoint not found",
+        status: 404
     })
 })
 
-app.listen(port,console.log(`server is running on http:/localhost:${port}`))
+app.listen(port, console.log(`server is running on http:/localhost:${port}`))
